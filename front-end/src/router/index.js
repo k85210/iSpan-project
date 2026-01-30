de94a48b598e94217c0632fc5184f9e88b214c8c
SeanXiang0115 <a600sean@gmail.com>
2026-01-31 00:09:24 +0800

    update commit message

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 // 定義router
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomeView.vue'),
+  },
+  {
     path: '/login',
     name: 'Login',
     component: () => import('@/views/LoginView.vue'),
