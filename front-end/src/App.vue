0e3faba00833363f8a1e947472b19d1f468b7685
Anna <shan80519@gmail.com>
2026-02-01 22:36:25 +0800

    chore: 同步 main 分支最新元件與視圖檔案

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-// import { computed } from 'vue';
-// import { useRoute } from 'vue-router';
-// import DefaultLayout from '@/layouts/default.vue';
-// import BlankLayout from '@/layouts/blank.vue';
-import Navbar from '@/layouts/navbar.vue';
-// const route = useRoute();
-
-// const layout = computed(() => {
-//   if (route.meta.layout === 'blank') return BlankLayout;
-//   return DefaultLayout;
-// });
+import navbar from './layouts/navbar.vue';
 </script>
 
 <template>
-  <Navbar />
-  <!-- <component :is="layout">
-    <RouterView />
-  </component> -->
-<RouterView />
+  <navbar />
+
+  <RouterView />
 </template>
 
 <style scoped></style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,45 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-
-
 // 定義router
 const routes = [
   {
     path: '/login',
     component: () => import('@/views/LoginView.vue'),
     name: 'Login'
-  },
-
-  {
-    path: '/shopStore',
-    component: () => import('@/views/shopStore.vue'),
-    name: 'ShopStore'
-  },
-  {
-    path: '/cart',
-    component: () => import('@/views/shopCart.vue'),
-    name: 'ShopCart',
-    name: 'Login',
-    meta: { layout: 'blank' }
-  },
-  {
-    path: '/register',
-    component: () => import('@/views/RegisterView.vue'),
-    name: 'Register',
-    meta: { layout: 'blank' }
-  },
-  {
-    path: '/productsDetail/:id',
-    component: () => import('@/views/productsDetail.vue'),
-    name: 'productsDetail',
   }
-  ,
-  {
-    path: '/checkOut',
-    component: () => import('@/views/checkOut.vue'),
-    name: 'checkOut',
-  }
-
 ];
 
 // 建立router
